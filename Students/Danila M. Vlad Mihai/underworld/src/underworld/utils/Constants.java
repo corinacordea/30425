0797ee3e2f03b8096565c5b9de1be03e03f54df7
Danila Vlad-Mihai <vlad96mihai@gmail.com>
2016-10-19 20:31:48 +0300

    TheUnderworld

---
+++ Students/Danila M. Vlad Mihai/underworld/src/underworld/utils/Constants.java
@@ -0,0 +1,11 @@
+package underworld.utils;
+
+public class Constants {
+	
+	public static  String WORLD_NAME = "your_world_name";
+	public static  String PATH_TO_RULES_FILE = "your_path_to_rules_file";
+	public static int MINER_BACKPACK_SIZE = 5;
+	public static int MAP_RESOURCES_NUMBER= 10;
+	public static int NUMBER_OF_ROUNDS = 5;
+	
+}
